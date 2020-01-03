5d1350136a3f82d1098590b582934075e15b7236
Steve Quinn <steve@sq.net.au>
2020-01-03 15:37:24 +1100

    Adds default export for useLocalStorage

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useOnKeyPress } from "./useOnKeyPress";
 export { default as useIsInView } from "./useIsInView";
 export { default as useIsMounted } from './useIsMounted';
+export { default as useLocalStorage } from './useLocalStorage';
