e461081721722ced258452ae7f2f0a8245ac2f37
Steve Quinn <steve@sq.net.au>
2020-01-03 16:02:52 +1100

    Adds a useArray hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -2,3 +2,4 @@ export { default as useOnKeyPress } from "./useOnKeyPress";
 export { default as useIsInView } from "./useIsInView";
 export { default as useIsMounted } from './useIsMounted';
 export { default as useLocalStorage } from './useLocalStorage';
+export { default as useArray } from './useArray';
---
+++ src/hooks/useArray.js
@@ -0,0 +1,23 @@
+import { useState, useCallback } from 'react';
+
+const useArray = initial => {
+  const [value, setValue] = useState(initial);
+
+  return {
+    value,
+    setValue,
+    add: useCallback(a => setValue(v => [...v, a]), []),
+    clear: useCallback(() => setValue(() => []), []),
+    removeById: useCallback(id => {
+      setValue(arr => arr.filter(v => v && v.id !== id));
+    }, []),
+    removeIndex: useCallback(index => {
+      setValue(v => {
+        v.splice(index, 1);
+        return v;
+      });
+    }, [])
+  };
+}
+
+export default useArray;
